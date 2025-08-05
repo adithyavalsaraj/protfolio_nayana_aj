1a233a5f59e50e63d925e34378994c064df7f6e0
Adithya Valsaraj <adithya@adivals.com>
2025-08-05 23:52:48 +0530

    favicon and profile image update

--- app/layout.tsx
+++ app/layout.tsx
@@ -177,8 +177,8 @@ export default function RootLayout({
         />
 
         {/* Favicon and app icons */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicons/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
