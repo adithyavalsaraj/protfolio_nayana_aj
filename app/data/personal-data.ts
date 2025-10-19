8a5f947d9003fd1f384fb782900732a5a33d1bff
Aadhi <adithya@adivals.com>
2025-10-19 21:11:00 +0530

    updated linked in

--- app/data/personal-data.ts
+++ app/data/personal-data.ts
@@ -10,21 +10,21 @@ export const personalData = {
   // bioSub:
   //   'I was previously a DST INSPIRE Faculty Fellow (2021-2023) at the Indian Institute of Astrophysics. I received a Ph.D. in Astrophysics from NCRA-TIFR in 2020, for a thesis titled "Most energetic explosions and their environments"',
   profileImage: "/profile.png?height=200&width=200",
-  // social: [
-  //   {
-  //     id: "linkedin",
-  //     title: "LinkedIn",
-  //     url: "https://www.linkedin.com/in/nayana-a-j-29b726138",
-  //   },
-  //   {
-  //     id: "orcid",
-  //     title: "ORCID",
-  //     url: "https://orcid.org/0000-0002-8070-5400",
-  //   },
-  //   {
-  //     id: "scholar",
-  //     title: "Google Scholar",
-  //     url: "https://scholar.google.com/citations?user=63zBs60AAAAJ&hl=en",
-  //   },
-  // ],
+  social: [
+    {
+      id: "linkedin",
+      title: "LinkedIn",
+      url: "https://www.linkedin.com/in/nayana-a-j-29b726138",
+    },
+    // {
+    //   id: "orcid",
+    //   title: "ORCID",
+    //   url: "https://orcid.org/0000-0002-8070-5400",
+    // },
+    // {
+    //   id: "scholar",
+    //   title: "Google Scholar",
+    //   url: "https://scholar.google.com/citations?user=63zBs60AAAAJ&hl=en",
+    // },
+  ],
 };
